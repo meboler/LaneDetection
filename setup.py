78ef98f0a9948df0748c9c3c0963374779c15560
Matt Boler <meb0054@auburn.edu>
2019-12-09 13:19:14 -0800

    idk

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['lane_detection'],
+    packages=['LaneDetector'],
     package_dir={'': 'src'},
 )
 setup(**setup_args)
